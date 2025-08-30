86eba07a9f923ea7e1a4032e8b571c9efa4fc93f
Rishitshivam <164783543+Rishitshivam@users.noreply.github.com>
2025-08-30 23:48:48 +0530

    commer topper componet

--- app/components/landing/topper.tsx
+++ app/components/landing/topper.tsx
@@ -6,18 +6,15 @@ interface TopperProps {
 }
 
 const Topper: React.FC<TopperProps> = ({ text, className = '' }) => (
-    <div className={`relative w-full flex items-center justify-center ${className}`}>
+    <div className={`relative w-full flex items-center justify-left ${className}`}>
         <Image src="/Landing/footer-dock 1.svg" alt="Footer Dock" width={1920} height={200} className="w-full h-auto" />
-        <span className="text-center whitespace-nowrap"
+        <span
+            className="absolute inset-0 flex items-center justify-left text-center whitespace-nowrap font-trap font-bold leading-none text-transparent pl-55"
             style={{
-              WebkitTextStroke: '2.41px #EFEFEF',
-              fontFamily: 'Trap, Arial, sans-serif',
-              fontSize: 'clamp(40px, 6vw, 77px)',
-              fontWeight: '100',
-              lineHeight: '100%',
-              color: 'transparent',
-              textAlign: 'center'
-            }}>
+                WebkitTextStroke: '2.41px #EFEFEF',
+                fontSize: 'clamp(40px, 6vw, 77px)',
+            }}
+        >
             {text}
         </span>
     </div>
