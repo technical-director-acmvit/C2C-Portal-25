5b3436568cb45f98aac54b2d2d4943e382960cf1
Rishitshivam <164783543+Rishitshivam@users.noreply.github.com>
2025-08-31 11:14:48 +0530

    updt

--- app/page.tsx
+++ app/page.tsx
@@ -4,22 +4,31 @@ import Landing from "./components/landing/landing";
 import { signIn } from "next-auth/react";
 import About from "./components/landing/About";
 import Topper from "./components/landing/topper";
-
+import Tracks from "./components/landing/tracks";
+import Speaker from "./components/landing/speaker";
 export default function Page() {
   return (
     <>
       <Landing />
+
+      
+
       <Topper text="About C2C" />
+      
 
       <About />
       <button
         onClick={() => signIn('google', { callbackUrl: '/portal' })}
-        className="fixed bottom-6 right-6 px-4 py-2 rounded-md bg-[#48BA86] text-black font-semibold shadow-lg hover:opacity-90"
+        className="fixed bottom-6 right-3 px-4 py-2 rounded-md bg-[#48BA86] text-black font-semibold shadow-lg hover:opacity-90"
         style={{ zIndex: 50 }}
         aria-label="Apply Now"
       >
         Apply Now
       </button>
+
+      <Speaker />
+      <Tracks />
+      <About />
     </>
 
   );
