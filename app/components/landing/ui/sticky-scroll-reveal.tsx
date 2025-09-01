abbd5cc7aad83bd67569c277d46c14ce4250fc95
theg <52027622+theg1239@users.noreply.github.com>
2025-09-02 04:11:05 +0530

    fix lint

--- app/components/landing/ui/sticky-scroll-reveal.tsx
+++ app/components/landing/ui/sticky-scroll-reveal.tsx
@@ -15,7 +15,7 @@ export const StickyScroll = ({
   content: {
     title: string;
     description: string;
-    content?: React.ReactNode | any;
+    content?: React.ReactNode;
   }[];
   contentClassName?: string;
 }) => {
