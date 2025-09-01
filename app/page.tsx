f952e2047beff30514137c233f28d839ea28feaf
theg <52027622+theg1239@users.noreply.github.com>
2025-09-01 19:05:51 +0530

    feat: refactor Desktop Register CTA to use ViewportPortal for improved positioning

--- app/page.tsx
+++ app/page.tsx
@@ -43,21 +43,24 @@ export default function Page() {
         <TopBar />
       </ViewportPortal>
 
-      {/* Smooth scrolling content wrapper (GSAP ScrollSmoother) */}
-      <div id="smooth-wrapper" className="relative z-0">
-        <div id="smooth-content">
-          {/* Landing - wrapped to overlay CTA on desktop */}
-          <div className="relative">
-            <Landing />
-            {/* Desktop CTA overlay - moves with page content */}
-            <div className="hidden md:flex absolute left-1/2 -translate-x-1/2 bottom-[8%] z-40">
+      {/* Desktop Register CTA fixed to viewport via portal (mobile CTA stays in Landing) */}
+      <ViewportPortal>
+        <div className="hidden md:flex fixed left-1/2 -translate-x-1/2 bottom-[8%] z-[9999]">
           <InteractiveHoverButton
             onClick={() => signIn("google", { callbackUrl: "/portal" })}
             className="w-[280px] text-lg px-5 py-2 min-h-[48px] rounded-full font-bold flex items-center justify-center bg-[#48BA86] hover:bg-[#3aa874] text-black border border-[#48BA86] transition-colors"
           >
             Register
           </InteractiveHoverButton>
-            </div>
+        </div>
+      </ViewportPortal>
+
+      {/* Smooth scrolling content wrapper (GSAP ScrollSmoother) */}
+      <div id="smooth-wrapper" className="relative z-0">
+        <div id="smooth-content">
+          {/* Landing - wrapped to overlay CTA on desktop */}
+          <div className="relative">
+            <Landing />
           </div>
 
           <div className="min-h-screen flex flex-col">
